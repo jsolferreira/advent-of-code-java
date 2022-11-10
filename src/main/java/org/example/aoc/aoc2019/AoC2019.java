b5960bb929a149e7b39e50ece98082879916c710
jsolferreira <jsolferreira@gmail.com>
2022-11-10 07:59:24 +0000

    2019

---
+++ src/main/java/org/example/aoc/aoc2019/AoC2019.java
@@ -0,0 +1,28 @@
+package org.example.aoc.aoc2019;
+
+import org.example.base.AoCYear;
+import org.example.base.Runnable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class AoC2019 extends AoCYear {
+
+    @Override
+    protected List<Class<? extends Runnable>> getDays() {
+
+        return List.of(
+                Day01.class,
+                Day02.class
+        );
+    }
+
+    @Override
+    protected Runnable newDayInstance(Class<? extends Runnable> c) throws NoSuchMethodException,
+                                                                          InvocationTargetException,
+                                                                          InstantiationException,
+                                                                          IllegalAccessException {
+
+        return c.getDeclaredConstructor().newInstance();
+    }
+}
