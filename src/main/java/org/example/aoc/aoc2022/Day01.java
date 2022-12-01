fa448c263c8cda2ad47f968f0f30b19fdd08ec64
jsolferreira <jsolferreira@gmail.com>
2022-12-01 08:34:37 +0000

    2022 Day 01

--- src/main/java/org/example/aoc/aoc2022/Day01.java
+++ src/main/java/org/example/aoc/aoc2022/Day01.java
@@ -3,33 +3,38 @@ package org.example.aoc.aoc2022;
 import java.util.Arrays;
 import java.util.List;
 
-class Day01 extends AoC2022Day<List<List<Integer>>> {
+class Day01 extends AoC2022Day<List<List<Long>>> {
 
     @Override
-    protected List<List<Integer>> parseInput(String strInput) {
+    protected List<List<Long>> parseInput(String strInput) {
 
         return Arrays.stream(strInput.split(System.lineSeparator() + System.lineSeparator()))
-                .map(group -> group.lines().map(Integer::parseInt).toList())
+                .map(group -> group.lines().map(Long::parseLong).toList())
                 .toList();
     }
 
     @Override
-    protected long partOne(List<List<Integer>> input) {
+    protected long partOne(List<List<Long>> input) {
 
         return input.stream()
-                .mapToInt(calories -> calories.stream().reduce(0, Integer::sum))
+                .mapToLong(this::sumCalories)
                 .max()
                 .orElseThrow();
     }
 
     @Override
-    protected long partTwo(List<List<Integer>> input) {
+    protected long partTwo(List<List<Long>> input) {
 
         return input.stream()
-                .mapToInt(calories -> calories.stream().reduce(0, Integer::sum))
+                .mapToLong(this::sumCalories)
                 .sorted()
                 .skip(input.size() - 3L)
                 .sum();
+    }
+
+    private long sumCalories(List<Long> calories) {
+
+        return calories.stream().reduce(0L, Long::sum);
     }
 
     @Override
