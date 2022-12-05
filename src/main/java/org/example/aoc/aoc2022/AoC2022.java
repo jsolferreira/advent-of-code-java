0c9b2abc54887709f9358a5e48f35ba0708bf400
jsolferreira <jsolferreira@gmail.com>
2022-12-05 09:21:33 +0000

    AoC2022 Day4

--- src/main/java/org/example/aoc/aoc2022/AoC2022.java
+++ src/main/java/org/example/aoc/aoc2022/AoC2022.java
@@ -14,7 +14,8 @@ public class AoC2022 extends AoCYear {
         return List.of(
                 Day01.class,
                 Day02.class,
-                Day03.class
+                Day03.class,
+                Day04.class
         );
     }
 
