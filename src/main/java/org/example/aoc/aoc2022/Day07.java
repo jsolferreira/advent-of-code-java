caf2c923ced2de9170da014cd141dc141b3d90bf
jsolferreira <jsolferreira@gmail.com>
2022-12-07 10:39:29 +0000

    AOC2022 day7

--- src/main/java/org/example/aoc/aoc2022/Day07.java
+++ src/main/java/org/example/aoc/aoc2022/Day07.java
@@ -2,21 +2,20 @@ package org.example.aoc.aoc2022;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
 
-class Day07 extends AoC2022Day<List<Record>> {
+class Day07 extends AoC2022Day<Day07.Directory> {
 
-    private record File(String name, int size) {
+    private record File(String name, long size) {
     }
 
-    private record Directory2(String name) {
-    }
-
-    private class Directory {
+    protected static class Directory {
 
         String name;
         Directory previous = null;
@@ -29,20 +28,23 @@ class Day07 extends AoC2022Day<List<Record>> {
         }
     }
 
-    private record ListDirectory(List<Record> list) {
+    private interface Instruction {
     }
 
-    private record ChangeDirectory(String name) {
+    private record ListDirectory(List<Directory> directories, List<File> files) implements Instruction {
+    }
+
+    private record ChangeDirectory(String name) implements Instruction {
     }
 
     @Override
-    protected List<Record> parseInput(String strInput) {
+    protected Directory parseInput(String strInput) {
 
         final Pattern cdPattern = Pattern.compile(".*cd (.*)");
         final Pattern directoryPattern = Pattern.compile("dir (.*)");
         final Pattern filePattern = Pattern.compile("(\\d+) (.*)");
 
-        return Arrays.stream(strInput.split("\\$ "))
+        final List<? extends Instruction> input = Arrays.stream(strInput.split("\\$ "))
                 .filter(not(String::isBlank))
                 .map(line -> {
 
@@ -53,143 +55,109 @@ class Day07 extends AoC2022Day<List<Record>> {
                         return new ChangeDirectory(cdMatcher.group(1));
                     }
 
-                    final List<Record> records = line.lines()
+                    final List<Directory> directories = line.lines()
                             .skip(1)
-                            .map(x -> {
-
-                                final Matcher directoryMatcher = directoryPattern.matcher(x);
-
-                                if (directoryMatcher.find()) {
-
-                                    return new Directory2(directoryMatcher.group(1));
-                                }
-
-                                final Matcher fileMatcher = filePattern.matcher(x);
-
-                                if (fileMatcher.find()) {
-
-                                    return new File(fileMatcher.group(2), Integer.parseInt(fileMatcher.group(1)));
-                                }
-
-                                throw new RuntimeException();
-                            })
+                            .map(directoryPattern::matcher)
+                            .filter(Matcher::find)
+                            .map(matcher -> new Directory(matcher.group(1)))
                             .toList();
 
-                    return new ListDirectory(records);
+                    final List<File> files = line.lines()
+                            .skip(1)
+                            .map(filePattern::matcher)
+                            .filter(Matcher::find)
+                            .map(matcher -> new File(matcher.group(2), Long.parseLong(matcher.group(1))))
+                            .toList();
+
+                    return new ListDirectory(directories, files);
                 })
                 .toList();
-    }
-
-    @Override
-    protected Long partOne(List<Record> input) {
 
         final Directory root = new Directory("/");
 
-        auxRec(root, input, 1);
+        parseDirectories(root, input, 1);
 
-        List<Directory> flat = flat(root);
-
-        return flat.stream()
-                .mapToLong(this::size)
-                .filter(size -> size < 100000)
-                .sum();
+        return root;
     }
 
-    private void auxRec(Directory currentDirectory, List<Record> instructions, int index) {
+    private void parseDirectories(Directory currentDirectory, List<? extends Instruction> instructions, int index) {
 
         if (index >= instructions.size()) {
 
             return;
         }
 
-        final Record record = instructions.get(index);
+        final Instruction instruction = instructions.get(index);
 
-        if (record instanceof ChangeDirectory changeDirectory) {
+        if (instruction instanceof ChangeDirectory changeDirectory) {
 
             if (changeDirectory.name.equals("..")) {
 
-                auxRec(currentDirectory.previous, instructions, index + 1);
+                parseDirectories(currentDirectory.previous, instructions, index + 1);
             } else {
 
-                Directory directory1 = currentDirectory.directories.stream()
-                        .filter(directory -> directory.name.equals(changeDirectory.name))
+                final Directory directory = currentDirectory.directories.stream()
+                        .filter(subDirectory -> subDirectory.name.equals(changeDirectory.name))
                         .findFirst()
                         .orElseThrow();
 
-                directory1.previous = currentDirectory;
+                directory.previous = currentDirectory;
 
-                auxRec(directory1, instructions, index + 1);
+                parseDirectories(directory, instructions, index + 1);
             }
-        } else if (record instanceof ListDirectory listDirectory) {
+        } else if (instruction instanceof ListDirectory listDirectory) {
 
-            for (Record record1 : listDirectory.list) {
+            currentDirectory.directories.addAll(listDirectory.directories);
+            currentDirectory.files.addAll(listDirectory.files);
 
-                if (record1 instanceof Directory2 directory2) {
-
-                    Directory directory = new Directory(directory2.name);
-                    currentDirectory.directories.add(directory);
-                } else if (record1 instanceof File file) {
-
-                    currentDirectory.files.add(file);
-                }
-            }
-
-            auxRec(currentDirectory, instructions, index + 1);
+            parseDirectories(currentDirectory, instructions, index + 1);
         }
-    }
-
-    private List<Directory> flat(Directory directory) {
-
-        if (directory.directories.isEmpty()) {
-
-            return List.of(directory);
-        }
-
-        ArrayList<Directory> directories = new ArrayList<>();
-
-        directories.add(directory);
-        for (Directory directory1 : directory.directories) {
-
-            directories.addAll(flat(directory1));
-        }
-
-        return directories;
-    }
-
-    private int size(Directory directory) {
-
-        int sum = 0;
-
-        for (File file : directory.files) {
-            sum += file.size;
-        }
-
-        for (Directory directory1 : directory.directories) {
-
-            sum += size(directory1);
-        }
-
-        return sum;
     }
 
     @Override
-    protected Long partTwo(List<Record> input) {
+    protected Long partOne(Directory input) {
 
-        final Directory root = new Directory("/");
+        return flat(input).stream()
+                .mapToLong(this::size)
+                .filter(size -> size < 100000)
+                .sum();
+    }
 
-        auxRec(root, input, 1);
+    @Override
+    protected Long partTwo(Directory input) {
 
-        int unusedSpace = size(root);
-        int toDelete = 30000000 - (70000000 - unusedSpace);
+        long toDelete = 30000000 - (70000000 - size(input));
 
-        List<Directory> flat = flat(root);
-
-        return flat.stream()
+        return flat(input).stream()
                 .mapToLong(this::size)
                 .filter(size -> size >= toDelete)
                 .sorted()
                 .findFirst()
                 .orElseThrow();
+    }
+
+    private List<Directory> flat(Directory directory) {
+
+        return Stream.concat(
+                        Stream.of(directory),
+                        directory.directories.stream()
+                                .map(this::flat)
+                                .flatMap(Collection::stream)
+                )
+                .toList();
+    }
+
+    private long size(Directory directory) {
+
+        final long filesSize = directory.files.stream()
+                .mapToLong(File::size)
+                .sum();
+
+        final long directoriesSize = directory.directories.stream()
+                .mapToLong(this::size)
+                .sum();
+
+        return filesSize + directoriesSize;
     }
 
     @Override
