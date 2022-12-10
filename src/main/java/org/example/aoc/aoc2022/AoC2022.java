c1851ec5ae701d5965155ea0e5ed09a8c2adc30c
Jorge <j.solferreira@gmail.com>
2022-12-10 09:21:35 +0000

    2022 day10

--- src/main/java/org/example/aoc/aoc2022/AoC2022.java
+++ src/main/java/org/example/aoc/aoc2022/AoC2022.java
@@ -27,7 +27,8 @@ public class AoC2022 extends AoCYear {
                 Day06.class,
                 Day07.class,
                 Day08.class,
-                Day09.class
+                Day09.class,
+                Day10.class
         );
     }
 
