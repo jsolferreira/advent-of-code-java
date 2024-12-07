8b7f9b7dafa3fcee51daeda49a8e95c82ab8a1e8
Jorge <j.solferreira@gmail.com>
2024-12-07 11:02:49 +0000

    AoC 2024 - Day 07

--- src/main/java/org/example/aoc/aoc2024/Day07.java
+++ src/main/java/org/example/aoc/aoc2024/Day07.java
@@ -8,7 +8,6 @@ import java.util.function.LongBinaryOperator;
 
 class Day07 extends AoC2024Day<List<Pair<Long, List<Long>>>> {
 
-
     @Override
     protected List<Pair<Long, List<Long>>> parseInput(String strInput) {
 
@@ -26,71 +25,35 @@ class Day07 extends AoC2024Day<List<Pair<Long, List<Long>>>> {
     @Override
     protected Long partOne(List<Pair<Long, List<Long>>> input) {
 
-        long total = 0;
-
-        for (Pair<Long, List<Long>> pair : input) {
-            Long testValue = pair.left();
-            List<Long> numbers = pair.right();
-
-            boolean p1 = applyOperator(testValue, numbers.getFirst(), numbers, 1, Long::sum);
-            boolean p2 = applyOperator(testValue, numbers.getFirst(), numbers, 1, (a, b) -> a * b);
-
-            if(p1 || p2) {
-                total += testValue;
-            }
-        }
-
-        return total;
-    }
-
-    private boolean applyOperator(long testValue, long previousNumber, List<Long> numbers, int index, LongBinaryOperator func) {
-
-        final long currentNumber = numbers.get(index);
-
-        final long nextNumber = func.applyAsLong(previousNumber, currentNumber);
-
-        if (index + 1 == numbers.size()) {
-            return nextNumber == testValue;
-        }
-
-        return applyOperator(testValue, nextNumber, numbers, index + 1, Long::sum) ||
-                applyOperator(testValue, nextNumber, numbers, index + 1, (a, b) -> a * b);
+        return run(input, List.of(Long::sum,(a, b) -> a * b));
     }
 
     @Override
     protected Long partTwo(List<Pair<Long, List<Long>>> input) {
 
-        long total = 0;
-
-        for (Pair<Long, List<Long>> pair : input) {
-            Long testValue = pair.left();
-            List<Long> numbers = pair.right();
-
-            boolean p1 = test2(testValue, numbers.getFirst(), numbers, 1, Long::sum);
-            boolean p2 = test2(testValue, numbers.getFirst(), numbers, 1, (a, b) -> a * b);
-            boolean p3 = test2(testValue, numbers.getFirst(), numbers, 1, this::concat);
-
-            if(p1 || p2 || p3) {
-                total += testValue;
-            }
-        }
-
-        return total;
+        return run(input, List.of(Long::sum,(a, b) -> a * b, this::concat));
     }
 
-    private boolean test2(long testValue, long previousNumber, List<Long> numbers, int index, LongBinaryOperator func) {
+    private long run(List<Pair<Long, List<Long>>> input, List<LongBinaryOperator> funcs) {
+
+        return input.stream()
+                .filter(pair -> applyOperator(pair.left(), pair.right().getFirst(), pair.right(), 1, funcs))
+                .mapToLong(Pair::left)
+                .sum();
+    }
+
+    private boolean applyOperator(long testValue, long previousNumber, List<Long> numbers, int index, List<LongBinaryOperator> funcs) {
+
+        if (index == numbers.size()) {
+            return previousNumber == testValue;
+        }
 
         final long currentNumber = numbers.get(index);
 
-        final long nextNumber = func.applyAsLong(previousNumber, currentNumber);
-
-        if (index + 1 == numbers.size()) {
-            return nextNumber == testValue;
-        }
-
-        return test2(testValue, nextNumber, numbers, index + 1, Long::sum) ||
-                test2(testValue, nextNumber, numbers, index + 1, (a, b) -> a * b) ||
-                test2(testValue, nextNumber, numbers, index + 1, this::concat);
+        return funcs
+                .stream()
+                .map(func -> func.applyAsLong(previousNumber, currentNumber))
+                .anyMatch(nextNumber -> applyOperator(testValue, nextNumber, numbers, index + 1, funcs));
     }
 
     private Long concat(long a, long b) {
