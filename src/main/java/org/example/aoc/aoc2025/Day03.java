cca8d6f5331b9e4983a80916bf3d4c79a8173efa
jsolferreira <jsolferreira@gmail.com>
2025-12-03 10:06:17 +0000

    AoC 2025 - Day 03

--- src/main/java/org/example/aoc/aoc2025/Day03.java
+++ src/main/java/org/example/aoc/aoc2025/Day03.java
@@ -1,6 +1,8 @@
 package org.example.aoc.aoc2025;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Day03 extends AoC2025Day<List<String>> {
 
@@ -13,37 +15,57 @@ class Day03 extends AoC2025Day<List<String>> {
     @Override
     protected Long partOne(List<String> input) {
 
-        long sum = 0;
-
-        for (String string : input) {
-
-            char c1 = string.charAt(0);
-            char c2 = string.charAt(1);
-
-            for (int i = 2; i < string.length(); i++) {
-
-                char c = string.charAt(i);
-
-                if (c2 > c1) {
-                    c1 = c2;
-                    c2 = c;
-                }
-
-                if (c > c2) {
-                    c2 = c;
-                }
-            }
-
-            sum += Long.parseLong("" + c1 + c2);
-        }
-
-        return sum;
+        return input.stream()
+                .map(s -> findJoltage(s, 2))
+                .reduce(Long::sum)
+                .orElse(0L);
     }
 
     @Override
-    protected Integer partTwo(List<String> input) {
+    protected Long partTwo(List<String> input) {
 
-        return null;
+        return input.stream()
+                .map(s -> findJoltage(s, 12))
+                .reduce(Long::sum)
+                .orElse(0L);
+    }
+
+    private long findJoltage(String bank, int nBatteries) {
+
+        final Character[] chars = bank.substring(0, nBatteries).chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        final int lastIndex = nBatteries - 1;
+
+        for (int i = nBatteries; i < bank.length(); i++) {
+
+            char c = bank.charAt(i);
+            int j = 0;
+
+            for (; j < chars.length - 1; j++) {
+                char c1 = chars[j];
+                char c2 = chars[j + 1];
+
+                if (c2 > c1) {
+                    shiftLeft(chars, j);
+                    chars[lastIndex] = c;
+                    break;
+                }
+            }
+
+            if (chars[lastIndex] < c) {
+                chars[lastIndex] = c;
+            }
+        }
+
+        final String collect = Arrays.stream(chars).map(String::valueOf).collect(Collectors.joining());
+
+        return Long.parseLong(collect);
+    }
+
+    private void shiftLeft(Character[] chars, int index) {
+
+        for (int i = index; i < chars.length - 1; i++) {
+            chars[i] = chars[i + 1];
+        }
     }
 
     @Override
