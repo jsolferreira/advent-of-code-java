5eb41f433bfe30f3d13e1b6844f09f7c2c05609e
Jorge <j.solferreira@gmail.com>
2025-12-06 12:23:29 +0000

    AoC 2025 - Day 06

--- src/main/java/org/example/aoc/aoc2025/Day06.java
+++ src/main/java/org/example/aoc/aoc2025/Day06.java
@@ -1,7 +1,5 @@
 package org.example.aoc.aoc2025;
 
-import org.example.aoc.utils.Pair;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,72 +7,51 @@ import java.util.function.LongBinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class Day06 extends AoC2025Day<Pair<char[][][], Character[]>> {
+class Day06 extends AoC2025Day<List<Day06.Problem>> {
+
+    protected record Problem(char[][] numbers, LongBinaryOperator op) {}
 
     @Override
-    protected Pair<char[][][], Character[]> parseInput(String strInput) {
+    protected List<Problem> parseInput(String strInput) {
 
         final String[] split = strInput.split(System.lineSeparator());
 
-        final String operationLine = split[split.length - 1];
+        final String firstLine = split[0];
+        int from = 0;
 
-        final Character[] operations = Arrays.stream(operationLine.split("\\s+"))
-                .map(s -> s.charAt(0))
-                .toArray(Character[]::new);
+        final List<Problem> problems = new ArrayList<>();
 
-        final char[] operationChars = operationLine.toCharArray();
-        int nSpaces = 0;
+        for (int i = 0; i < firstLine.length(); i++) {
+            final char c = firstLine.charAt(i);
 
-        int pos = 0;
+            if (c == ' ') {
+                int finalI = i;
+                boolean isEndOfProblem = Arrays.stream(split).allMatch(s -> s.charAt(finalI) == ' ');
 
-        List<List<String>> xxx = new ArrayList<>();
-
-        for (int i = 1; i <= operationChars.length; i++) {
-            if (i == operationChars.length || operationChars[i] != ' ') {
-
-                final List<String> chunks = new ArrayList<>();
-
-                for (int ii = 0; ii < split.length - 1; ii++) {
-                    final String row = split[ii];
-
-                    final String chunk = nSpaces == 0
-                            ? row.substring(pos)
-                            : row.substring(pos, pos + nSpaces);
-
-                    chunks.add(chunk);
+                if (isEndOfProblem) {
+                    final Problem problem = extractProblem(split, from, i);
+                    problems.add(problem);
+                    from = i + 1;
                 }
-
-                xxx.add(chunks);
-                pos += nSpaces + 1;
-                nSpaces = 0;
-            } else {
-
-                nSpaces++;
             }
         }
+        final Problem problem = extractProblem(split, from, firstLine.length());
+        problems.add(problem);
 
-        final char[][][] c = xxx.stream()
-                .map(xx -> xx.stream()
-                        .map(String::toCharArray)
-                        .toArray(char[][]::new))
-                .toArray(char[][][]::new);
-
-        return new Pair<>(c, operations);
+        return problems;
     }
 
     @Override
-    protected Long partOne(Pair<char[][][], Character[]> input) {
-
-        final char[][][] numbers = input.left();
-        final Character[] operations = input.right();
+    protected Long partOne(List<Problem> input) {
 
         long sum = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            final LongBinaryOperator op = getOperation(operations[i]);
+        for (Problem problem : input) {
 
-            sum += Arrays.stream(numbers[i]).mapToLong(n -> Long.parseLong(String.valueOf(n).trim()))
-                    .reduce(op)
+            final char[][] numbers = problem.numbers();
+
+            sum += Arrays.stream(numbers).mapToLong(n -> Long.parseLong(String.valueOf(n).trim()))
+                    .reduce(problem.op())
                     .orElse(0);
         }
 
@@ -82,28 +59,35 @@ class Day06 extends AoC2025Day<Pair<char[][][], Character[]>> {
     }
 
     @Override
-    protected Long partTwo(Pair<char[][][], Character[]> input) {
-
-        final char[][][] numbers = input.left();
-        final Character[] operations = input.right();
+    protected Long partTwo(List<Problem> input) {
 
         long sum = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            final LongBinaryOperator op = getOperation(operations[i]);
+        for (Problem problem : input) {
 
-            final char[][] number = numbers[i];
+            final char[][] numbers = problem.numbers();
 
-            sum += IntStream.range(0, number[0].length)
-                    .mapToObj(ii -> Arrays.stream(Arrays.stream(number).map(chars -> chars[ii]).toArray())
+            sum += IntStream.range(0, numbers[0].length)
+                    .mapToObj(ii -> Arrays.stream(Arrays.stream(numbers).map(chars -> chars[ii]).toArray())
                             .map(String::valueOf)
                             .collect(Collectors.joining()))
                     .map(s -> Long.parseLong(s.trim()))
-                    .reduce(op::applyAsLong)
+                    .reduce(problem.op::applyAsLong)
                     .orElse(0L);
         }
 
         return sum;
+    }
+
+    private Problem extractProblem(String[] input, int from, int to) {
+
+        final char op = input[input.length - 1].charAt(from);
+
+        final char[][] numbers = IntStream.range(0, input.length - 1)
+                .mapToObj(i -> input[i].substring(from, to).toCharArray())
+                .toArray(char[][]::new);
+
+        return new Problem(numbers, getOperation(op));
     }
 
     private LongBinaryOperator getOperation(char op) {
